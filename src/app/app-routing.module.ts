e9fea7123d2cd1cbbfdc15840bfce5578eac9250
A_souny <maanramirezsa@ittepic.edu.mx>
2020-04-27 21:42:11 -0600

    Segundo Commit
    El usuario y contraseña son
    
    Maanramirezsa@ittepic.edu.mx
    prueba123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login',  pathMatch: 'full'},
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'view-student',
+    loadChildren: () => import('./pages/view-student/view-student.module').then( m => m.ViewStudentPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
